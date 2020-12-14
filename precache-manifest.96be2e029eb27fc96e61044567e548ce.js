256df2f6d0e50437658c4b58449cced117671047
Aditya9111 <52771727+Aditya9111@users.noreply.github.com>
2020-12-14 09:54:49 +0530

    Updates

---
+++ precache-manifest.96be2e029eb27fc96e61044567e548ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9b68166422f998e3149b4d95219e9b",
+    "url": "/Monsters-React/index.html"
+  },
+  {
+    "revision": "6ba04703fa0c6d7b7757",
+    "url": "/Monsters-React/static/css/main.e0afbac2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-React/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-React/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba04703fa0c6d7b7757",
+    "url": "/Monsters-React/static/js/main.de80879e.chunk.js"
+  },
+  {
+    "revision": "c55e07348823461716e1",
+    "url": "/Monsters-React/static/js/runtime-main.e72c00e9.js"
+  }
+]);
